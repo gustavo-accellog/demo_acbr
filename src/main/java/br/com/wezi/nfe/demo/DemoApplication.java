79bc8320b0d069423a6c5b8b411006f7d398bfa9
Gustavo Mantovani <gustavo@accellog.com>
2019-10-10 11:50:27 -0300

    Gravando senha pelo Java e já criptografando e mandando a informação da versão da nota para o arquivo .ini

--- src/main/java/br/com/wezi/nfe/demo/DemoApplication.java
+++ src/main/java/br/com/wezi/nfe/demo/DemoApplication.java
@@ -13,6 +13,14 @@ public class DemoApplication {
 		
 		try {
             acbrNFe = new ACBrNFe();
+            System.out.println("Versão " + acbrNFe.nome() + " = " + acbrNFe.versao());
+            acbrNFe.configGravarValor(ACBrSessao.DFe, "Senha", "informar senha");
+            acbrNFe.configGravarValor(ACBrSessao.NFe, "VersaoDF", "3");
+            System.out.println(acbrNFe.configLerValor(ACBrSessao.DFe, "Senha"));
+            
+            System.out.println(acbrNFe.statusServico());
+            
+            System.out.println("Fim do Teste");
         } catch (Exception ex) {
             System.out.println(ex);
         }
